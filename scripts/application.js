6f44c55fedd6f0bf51392ae2b864ee6be25be405
Daniel Grieve <git@dgrieve.com>
2013-11-03 18:00:05 +0000

    show/hide the scotland js 2013 video

--- scripts/application.js
+++ scripts/application.js
@@ -3,6 +3,10 @@ $(function () {
 
   navigation = $('header nav ul');
 
+  resizeVideo = function () {
+    $('.video iframe').attr('height', $('.banner').outerHeight());
+  };
+
   iconMenuTriggered = function (e) {
     e.preventDefault();
 
@@ -20,5 +24,28 @@ $(function () {
     }
   };
 
+  watchVideoTriggered = function (e) {
+    e.preventDefault();
+
+    if ($('.video').hasClass('show')) {
+      $('.video').fadeOut({
+        queue: false
+      }).removeClass('show');
+      $(this).removeClass('show');
+      $('.button-video').removeClass('hide');
+    } else {
+      $('.video').fadeIn({
+        queue: false
+      }).addClass('show');
+      $(this).addClass('hide');
+      $('.close-video').addClass('show');
+    }
+  };
+
   $('.icon-menu').bind(isMobile ? 'touchstart' : 'click', iconMenuTriggered);
+  $('.button-video').bind('click', watchVideoTriggered);
+  $('.close-video').bind('click', watchVideoTriggered);
+
+  $(window).resize(resizeVideo);
+  resizeVideo();
 });
