bd7053bca056eeb3f5485af2e5b2229094a475cd
jiggypete <jiggypete@gmail.com>
2018-06-27 11:59:27 +0100

    Site updated at 2018-06-27 10:59:27 UTC

--- play.rb
+++
@@ -1,7 +0,0 @@
-require 'yaml'
-data = YAML.load_file('../../scotlandcss/scotlandcss/_data/speakers.yml')
-speakers =  data
-speakers.each do |speaker|
-  image = speaker["image"][9..-1]
-  `open "http://localhost:5000/?conference=scotlandcss&name=#{speaker["name"]}&talk=#{speaker["title"]}&twitter=#{speaker["twitter"]}&picture=#{image}"`
-end
